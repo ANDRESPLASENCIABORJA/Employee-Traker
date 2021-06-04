fcb05ac5d013430a3e0c37ac1d0938bfa480b467
APB98 <borja.basolutions@gmail.com>
2021-06-04 01:39:03 -0500

    adition of console.table and modification to js first functions to view employees roles and departments also to add departments

--- employeeTraker.js
+++ employeeTraker.js
@@ -1,6 +1,7 @@
 // Call our dependencies inquirer and mysql
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // Create a connection  information to sql employeeTraker_DB
 const connection = mysql.createConnection({
@@ -49,51 +50,147 @@ const start = () => {
         });
 };
 
-// Function to handle with the add DEPARTMENT choice
-const departmentAuctionAdd = () => {
-
-};
-
-// Function to handle with the add ROLE choice
-const roleAuctionAdd = () => {
-
-};
-
-// Function to handle with the add EMPLOYEE choice
-const employeeAuctionAdd = () => {
-
-};
-
-// Function to handle with the add DEPARTMENT choice
-const departmentAuctionAdd = () => {
-
-};
-
-// Function to handle with the add ROLE choice
-const roleAuctionAdd = () => {
-
-};
-
-// Function to handle with the add EMPLOYEE choice
-const employeeAuctionAdd = () => {
-
-};
-
-// Function to handle with the add DEPARTMENT choice
+// Function to handle with the View all Departments choice
 const departmentAuctionView = () => {
+    connection.query('SELECT * from department', (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        console.table('Department:', data[0].name);
+        // Prompt the starting questions to the user
+        start();
+    });
+};
 
+// Function to handle with the View all ROLES choice
+const roleAuctionView = () => {
+    connection.query('SELECT * from role', (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        console.table('Role:', data[0].title);
+        // Prompt the starting questions to the user
+        start();
+    });
+};
+
+// Function to handle with the View all EMPLOYEE choice
+const employeeAuctionView = () => {
+    connection.query('SELECT * from employee', (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        console.table('Employee', data[0].first_name);
+        // Prompt the starting questions to the user
+        start();
+    });
+};
+
+// Function to handle with the Add DEPARTMENT choice
+const departmentAuctionAdd = () => {
+    // Prompt to store information from the user
+    inquirer
+        .prompt([
+            {
+                name: 'departmentName',
+                type: 'input',
+                message: 'What is the new department name?'
+            },
+        ])
+        .then((answer) => {
+            // When finished prompting, insert the new item into the db table
+            connection.query(
+                'INSERT INTO department SET ?',
+                {
+                    name: answer.departmentName,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your new department was successfully created');
+                    // Prompt the starting questions to the user
+                    start();
+                }
+            );
+        });
 };
 
 // Function to handle with the add ROLE choice
-const roleAuctionView = () => {
-
+const roleAuctionAdd = () => {
+    // Prompt to store information from the user
+    inquirer
+        .prompt([
+            {
+                name: 'roleTitle',
+                type: 'input',
+                message: 'What is the new role title?'
+            },
+            {
+                name: 'roleSalary',
+                type: 'input',
+                message: 'What is the new role salary?'
+            },
+            {
+                name: 'in which department does the role goes?',
+                type: 'input',
+                message: 'What is the new department name?'
+            },
+        ])
+        .then((answer) => {
+            // When finished prompting, insert the new item into the db table
+            connection.query(
+                'INSERT INTO department SET ?',
+                {
+                    name: answer.departmentName,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your new department was successfully created');
+                    // Prompt the starting questions to the user
+                    start();
+                }
+            );
+        });
 };
 
 // Function to handle with the add EMPLOYEE choice
-const employeeAuctionView = () => {
-
+const employeeAuctionAdd = () => {
+    // Prompt to store information from the user
+    inquirer
+        .prompt([
+            {
+                name: 'departmentName',
+                type: 'input',
+                message: 'What is the new department name?'
+            },
+            {
+                name: 'departmentName',
+                type: 'input',
+                message: 'What is the new department name?'
+            },
+            {
+                name: 'departmentName',
+                type: 'input',
+                message: 'What is the new department name?'
+            },
+            {
+                name: 'departmentName',
+                type: 'input',
+                message: 'What is the new department name?'
+            },
+        ])
+        .then((answer) => {
+            // When finished prompting, insert the new item into the db table
+            connection.query(
+                'INSERT INTO department SET ?',
+                {
+                    name: answer.departmentName,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your new department was successfully created');
+                    // Prompt the starting questions to the user
+                    start();
+                }
+            );
+        });
 };
-
 
 // Function to handle with the update EMPLOYEE ROLE choice
 const employeeAuctionUpdate = () => {
@@ -103,7 +200,7 @@ const employeeAuctionUpdate = () => {
 // Connect to the mysql server and sql database
 connection.connect((err) => {
     if (err) throw err;
-    // If the connection is successfull then log
-    console.log('There you go!');
+    // Run the start function after the connection is made to prompt the user
+    start();
 })
 
