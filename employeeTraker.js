ecb1bfb1d1a3f353461e6e10b05f05815fd1cba6
APB98 <borja.basolutions@gmail.com>
2021-06-04 00:36:36 -0500

    Starting function to the js file and skeleton for all the functions that will be working on the application

--- employeeTraker.js
+++ employeeTraker.js
@@ -17,8 +17,91 @@ const connection = mysql.createConnection({
     database: 'employeeTraker_DB',
 });
 
+// At first we have to create a starter function to welcome the user to the program
+// This function will have all the 3 choices that the user can take and when they chose one they will be redirected to the function of the selected choice
+const start = () => {
+    inquirer
+        .prompt({
+            name: 'homePage',
+            type: 'list',
+            message: 'Welcome to the Employee Traker app! What would you like to do today?',
+            choices: ['Add Department', 'Add Role', 'Add Employee', 'View all Departments', 'View all Roles', 'View all Employees', 'Update Employee Roles'],
+        })
+        .then((answer) => {
+            // Based on the user answer they can add a department with a department function or a role or an employee
+            if (answer.homePage === 'Add Department') {
+                departmentAuctionAdd();
+            } else if (answer.homePage === 'Add Role') {
+                roleAuctionAdd();
+            } else if (answer.homePage === 'Add Employee') {
+                employeeAuctionAdd();
+            } else if (answer.homePage === 'View all Departments') {
+                departmentAuctionView();
+            } else if (answer.homePage === 'View all Roles') {
+                roleAuctionView();
+            } else if (answer.homePage === 'View all Employees') {
+                employeeAuctionView();
+            } else if (answer.homePage === 'Update Employee Roles') {
+                employeeAuctionUpdate();
+            } else {
+                connection.end();
+            }
+        });
+};
+
+// Function to handle with the add DEPARTMENT choice
+const departmentAuctionAdd = () => {
+
+};
+
+// Function to handle with the add ROLE choice
+const roleAuctionAdd = () => {
+
+};
+
+// Function to handle with the add EMPLOYEE choice
+const employeeAuctionAdd = () => {
+
+};
+
+// Function to handle with the add DEPARTMENT choice
+const departmentAuctionAdd = () => {
+
+};
+
+// Function to handle with the add ROLE choice
+const roleAuctionAdd = () => {
+
+};
+
+// Function to handle with the add EMPLOYEE choice
+const employeeAuctionAdd = () => {
+
+};
+
+// Function to handle with the add DEPARTMENT choice
+const departmentAuctionView = () => {
+
+};
+
+// Function to handle with the add ROLE choice
+const roleAuctionView = () => {
+
+};
+
+// Function to handle with the add EMPLOYEE choice
+const employeeAuctionView = () => {
+
+};
+
+
+// Function to handle with the update EMPLOYEE ROLE choice
+const employeeAuctionUpdate = () => {
+
+};
+
 // Connect to the mysql server and sql database
-connection.connect((err)=> {
+connection.connect((err) => {
     if (err) throw err;
     // If the connection is successfull then log
     console.log('There you go!');
